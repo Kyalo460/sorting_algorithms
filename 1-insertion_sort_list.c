68e154e97171f2a35a91aac693935aded9165982
Kyalo460 <kyalo460@gmail.com>
2023-10-23 09:50:41 +0300

    The function does nothing

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,45 +1,12 @@
 #include "sort.h"
 
 /**
- * insert_node - inserts the node by swapping places.
- * @list: a double pointer to the head of list.
- * @swap: a double pointer to node to swap.
- * @insert: a pointer to node to insert.
- */
-void insert_node(listint_t **list, listint_t **swap, listint_t *insert)
-{
-	(*swap)->next = insert->next;
-	if (insert->next != NULL)
-		insert->next->prev = *swap;
-	insert->prev = (*swap)->prev;
-	insert->next = *swap;
-	if ((*swap)->prev != NULL)
-		(*swap)->prev->next = insert;
-	else
-		*list = insert;
-	(*swap)->prev = insert;
-	*swap = insert->prev;
-}
-
-/**
- * insertion_sort_list - sorts a linked list using insertion sort
- * @list: a double pointer to the head of linked list
- */
+ * insertion_sort_list - returns without doing anything
+ * @list: a double pointer to the linked list
+*/
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *insert, *temp;
-
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (*list != NULL)
 		return;
-
-	for (current = (*list)->next; current != NULL; current = temp)
-	{
-		temp = current->next;
-		insert = current->prev;
-		while (insert != NULL && current->n < insert->n)
-		{
-			insert_node(list, &insert, current);
-			print_list((const listint_t *)*list);
-		}
-	}
+	return;
 }
